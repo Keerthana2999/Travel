8c8d6e046e5a12a466418e472b6117dffeea62b1
Keerthana2999 <66051436+Keerthana2999@users.noreply.github.com>
2021-09-05 17:42:55 +0530

    Add files via upload
    Estimation layout completed

---
+++ EstimationActivity.java
@@ -0,0 +1,13 @@
+package com.example.travellayout1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EstimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estimation);
+    }
+}
